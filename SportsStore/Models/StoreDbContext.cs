349a4fa1c1b277890ce34eafc3ddcb15e6948010
Sviatoslav <slavaua2312@gmail.com>
2024-10-17 16:42:22 +0300

    working version of shop

---
+++ SportsStore/Models/StoreDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options)
+            : base(options)
+        {
+        }
+
+#pragma warning disable SA1101 // Prefix local calls with this
+        public DbSet<Product> Products => Set<Product>();
+#pragma warning restore SA1101 // Prefix local calls with this
+
+#pragma warning disable SA1101 // Prefix local calls with this
+        public DbSet<Order> Orders => Set<Order>();
+#pragma warning restore SA1101 // Prefix local calls with this
+    }
+}
